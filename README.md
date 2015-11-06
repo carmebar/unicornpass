4b946148f29da0ce6a531078ba61e28075f45aed
Carlos Melero <carlosmelero@live.coim>
2015-11-06 12:34:10 +0100

    Added GreaseMonkey/TamperMonkey script.

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 #UnicornPass
 Firefox/Fennec extension.
 Colorful password fields using SHA1 hash of your password. You will know if you typed the correct password before submitting.
+
+##GreaseMonkey compatibility
+You can use UnicornPass.user.js as a GreaseMonkey user content script. You can use TamperMonkey to use it in Chrome/Chromium.
+
+#Multiple Browsers/Computers
+You can interchange Firefox/Fennec extension with the GreaseMonkey/TamperMonkey script. The color output will be the same AS LONG as the Salt is the same. You can see the generated Salt for the Firefox/Fennec extension in the Settings panel. Add-ons->Extensions->UnicornPass->Preferences.
+
+The salt MUST BE secret.
+
+##Security note
+If you're worried about the colors of the password field revealing information continue reading.
+
+The colors are computed using SHA1 hash salted with a random salt calculated upon installation. That means that each installation of UnicornPass outputs different colors for the same password. If that wasn't the case someone would be able to build a table with the most common passwords and their color output (Rainbow table).
---
+++ UnicornPass.user.js
@@ -0,0 +1,53 @@
+// ==UserScript==
+// @name        UnicornPass
+// @namespace   unicornpass
+// @include     *
+// @version     0.1
+// @run-at      document-end
+// @require     https://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/sha1.js
+// @grant       GM_setValue
+// @grant       GM_getValue
+// ==/UserScript==
+var salt = ""; //Set manually if you want to override the generated one. USE WITH CAUTION
+               //Useful to have same color outputs from different browsers/computers
+
+
+if (!GM_getValue("unicorn_pass_salt", salt)){
+  salt = CryptoJS.SHA1(Math.random().toString()).lwords.map((a)=>a.toString(16)).join('');
+  GM_setValue("unicorn_pass_salt", salt);
+}
+
+salt = salt || GM_getValue("unicorn_pass_salt");
+
+window.addEventListener("keyup",function(event){
+  function solidGradient(colors){
+    if(colors.length == 1) return colors[0];
+    var s = 100/colors.length;
+
+    return 'linear-gradient(to right ,' + colors.map(function(v,i){
+      return v + " " + i*s + "%," + v + " " + (i+1)*s + "%"
+    }).join() + ')';
+  }
+
+  function dword2rgba(i){
+    return 'rgba(' + [(i>> 24) & 255, (i>> 16) & 255, (i>> 8) & 255, (i & 255)/255].join() + ')';
+  }
+  var e = event.target;
+  if (e.type.toLowerCase() === 'passwordd'){
+    if(e.value.length > 0){
+      var hash  = CryptoJS.SHA1(salt+e.value).words;
+      var xor   = hash.reduce((a,b)=>a^b, 0);
+      
+      if(typeof e.__old_style === "undefined")
+        e.__old_style = e.style;
+
+      e.style.setProperty('background', dword2rgba(xor), 'important');
+      e.style.setProperty('background-image', solidGradient(hash.map(dword2rgba)), 'important');
+      e.style.setProperty('background-repeat', 'unset', 'important');
+      e.style.setProperty('box-shadow', 'unset', 'important');
+    }else{
+      e.style = e.__old_style;
+      delete e.__old_style;
+    }
+  }
+},false);
