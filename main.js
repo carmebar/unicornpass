471acd44dcd3864ef0c98a9ceb2a7027c3109bba
Carlos Melero <carlosmelero@live.com>
2017-08-15 00:46:12 +0200

    Fixed #7

--- main.js
+++ main.js
@@ -2,9 +2,32 @@
 
 var browser = typeof chrome === 'undefined'? browser : chrome;
 
+function getSalt(){
+  return new Promise((res, _) => {
+    if (typeof chrome !== 'undefined'){
+      browser.storage.local.get('salt', (r) => { res(r.salt); }); //Chrome uses callbacks
+    }else{
+      browser.storage.local.get('salt').then( (r) => {
+        if (Array.isArray && Array.isArray(r))
+          r = r[0]; //Workaround for FF < 52
+        res(r.salt);
+      });
+    }
+  });
+}
+
+function setRandomSalt(){
+  browser.storage.local.set({
+    salt: '{x-x-x}'.replace(/x/g,()=>crypto.getRandomValues(new Uint32Array(1))[0].toString(16))
+  });
+}
+
 browser.runtime.onInstalled.addListener((details) => {
   if (details.reason === 'install')
-    browser.storage.local.set({
-      salt: '{x-x-x}'.replace(/x/g,()=>crypto.getRandomValues(new Uint32Array(1))[0].toString(16))
+    setRandomSalt();
+  else if (details.reason === 'update')
+    getSalt().then((salt) => {
+      if (typeof salt === 'undefined')
+        setRandomSalt();
     });
 });
